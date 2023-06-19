d6390efa06de731cd7f9455a7a3daf4393a3ca2a
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-19 21:28:46 +0200

    import reactive form module && create login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import {FormGroup} from '@angular/forms'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+data: any;
+  constructor () {}
+loginForm !: FormGroup |any ;
+  ngOnInit(): void {
+  }
 
+  login(data :FormGroup|any) {
+  }
 }
