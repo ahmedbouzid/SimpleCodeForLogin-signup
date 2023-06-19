3aa17b9177f69df830d04b09b5e905d7984f3f77
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-19 22:08:48 +0200

    prepare sign up form && logic

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component  , OnInit} from '@angular/core';
+import {FormGroup ,FormControl} from '@angular/forms'
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
+signupForm !: FormGroup ;
+
+ngOnInit(): void {
+  this.signupForm = new FormGroup ({
+    'firstName' : new FormControl() ,
+    'lastName' : new FormControl() ,
+    'email': new FormControl() ,
+    'password' : new FormControl() ,
+  })
+}
+signup(data : FormGroup){
+  console.log(this.signupForm.value);
+
+}
 }
