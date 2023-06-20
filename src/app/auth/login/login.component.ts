1d35334f416357d74ddf6fd7d2cb56c0172ae6ec
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-20 10:44:20 +0200

    login logic

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component , OnInit} from '@angular/core';
 import {FormGroup ,FormControl} from '@angular/forms'
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,7 +10,9 @@ import {FormGroup ,FormControl} from '@angular/forms'
 })
 export class LoginComponent implements OnInit {
 data: any;
-  constructor () {}
+  constructor ( private http : HttpClient ,
+    private toast : ToastrService,
+    private router : Router) {}
 loginForm !: FormGroup |any ;
   ngOnInit(): void {
     this.loginForm = new FormGroup({
@@ -18,6 +23,27 @@ loginForm !: FormGroup |any ;
 
   login(data :FormGroup|any) {
     console.log(this.loginForm.value);
+    this.http.get<any>('http://localhost:8080/signup')
+    .subscribe(res => {
+      const user = res.find((a : any)=> {
+        return a.firstName === this.loginForm.value.firstName && a.password === this.loginForm.value.password
+      }) ;
+      if (user) {
+        const user = this.loginForm.value.firstName;
+        this.toast.success('Login Succfully' , user) ;
+
+        this.loginForm.reset()
+        this.router.navigate(['/dashboard'])
+      }
+      else {
+        this.toast.error(' icorrect Password or User name ')
+      }
+    } ,
+    err => {
+      console.log(err);
+
+    }
+    )
 
 
   }
